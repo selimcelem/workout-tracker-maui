ac455eacaa33f36f37ba24b90eb26fccd34d3e2c
selimcelem <selim.celem@gmail.com>
2025-08-25 11:57:30 +0200

    feat: create .NET MAUI app scaffold

---
+++ WorkoutTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WorkoutTracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WorkoutTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
